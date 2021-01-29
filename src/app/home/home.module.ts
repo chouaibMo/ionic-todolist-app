c7d08d52ec6c195a11d8427ac13c675b29cb995a
Chouaib Mounaime <chouaib.mounaime@etu.univ-grenoble-alpes.fr>
2021-01-29 09:09:15 +0100

    home page with generated values

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {CreateListComponent} from "../modals/create-list/create-list.component";
 
 
 @NgModule({
@@ -12,8 +13,12 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule,
   ],
-  declarations: [HomePage]
+  declarations: [
+      HomePage,
+      CreateListComponent,
+  ]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {List} from "../models/list";
+import {ListService} from "../services/list.service";
+import {ModalController} from "@ionic/angular";
+import {CreateListComponent} from "../modals/create-list/create-list.component";
+import {Todo} from "../models/todo";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    public lists: List[]
 
-  constructor() {}
+    constructor(private listService: ListService,
+                private modalController: ModalController) {
+        const nbLists = Math.floor(Math.random() * (40 - 20)) + 20;
+        for (let i=0; i<nbLists; i++){
+            this.listService.create("my list number"+ i);
+        }
+        this.lists = this.listService.getAll();
 
+        this.lists.forEach(function (list) {
+            const nb = Math.floor(Math.random() * (11));
+            for (let i=0; i<nb; i++){
+                list.addTodo(new Todo("my todo number i", "decription of my todo number i"))
+            }
+        });
+    }
+
+    ngOnInit(){
+    }
+
+    async presentModal(){
+        const modal = await this.modalController.create({
+            component: CreateListComponent,
+        });
+        return await modal.present();
+    }
+
+    delete(list: List): void {
+        this.listService.delete(list)
+    }
 }
