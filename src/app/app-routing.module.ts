c3cf54aac939798d2a9b53195a69da7b48fb60b8
Chouaib Mounaime <chouaib.mounaime@etu.univ-grenoble-alpes.fr>
2021-01-29 18:58:04 +0100

    adding register page linked to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 
 @NgModule({
